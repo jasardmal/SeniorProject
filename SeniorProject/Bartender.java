6266acd542c63068e29b3c37791be0a2ae8e417b
Jason <jasardmal@yahoo.com>
2015-12-02 07:06:07 -0800

    Identification Base

---
+++ SeniorProject/Bartender.java
@@ -0,0 +1,33 @@
+
+/**
+ * Write a description of class Bartender here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Bartender extends Staff
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class Bartender
+     */
+    public Bartender()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
